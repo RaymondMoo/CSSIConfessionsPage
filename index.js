2f36e6113814c40c0c7522883ed75facee099da7
Raymond Mo <raymondmo1545@gmail.com>
2020-07-28 21:04:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const DataStore = require('nedb');
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+
+
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new DataStore('database.db');
+database.loadDatabase();
+
+app.post('/api', (request,response) => {
+  console.log("request received");
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+
+  response.json({
+    status: "success",
+    latitude: data.lat,
+    longitude: data.lon,
+    post: data.post,
+    timestamp: timestamp
+  });
+});
---
+++ public/sketch.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('submit');
+button.addEventListener('click', async event =>{
+
+  const post = document.getElementById('post').value;
+  const data = {post};
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  const response = await fetch('/api', options);
+  const json = await response.json();
+  console.log(json);
+})
+
+
+
+
+
+// if('geolocation' in navigator){
+//   console.log('geolocation available');
+//   navigator.geolocation.getCurrentPosition(async (position) => {
+//       lat = position.coords.latitude;
+//       lon = position.coords.longitude;
+//       console.log(lat, lon);
+//       document.getElementById('latitude').textContent = lat;
+//       document.getElementById('longitude').textContent = lon;
+
+
+//     });
+// }else {
+//   console.log('geolocation is not available');
+
+// }
